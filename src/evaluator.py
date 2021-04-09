f2c283606bd879411fb4ca713b97638b5010ecae
egdegd <egd3700@mail.ru>
2021-04-09 15:45:38 +0300

    add doubly robust estimator

--- src/evaluator.py
+++ src/evaluator.py
@@ -20,24 +20,17 @@ class IPSEvaluator:
 
 class DoublyRobustEstimator:
 
-    def __init__(self, policy, history, model_based_estimator, mu, pi):
-        self.policy = policy
-        self.history = history
-        self.model_based_estimator = model_based_estimator(history)
-        self.mu = mu
-        self.pi = pi
+    def __init__(self, log_policy: BasePolicy, eval_policy: BasePolicy, model_based_estimator):
+        self.log_policy = log_policy
+        self.eval_policy = eval_policy
+        self.model_based_estimator = model_based_estimator
 
-    def get_action_and_reward(self, x_i):
-        for (x, a, r) in self.history:
-            if x == x_i:
-                return a, r
-
-    def evaluate_one_reward(self, x, a, real_a, real_r):
-        mb = self.model_based_estimator(x, a)
-        return mb + (real_r - mb) * self.pi(a, x) / self.mu(real_a, x)
+    def evaluate_one_reward(self, x, a, r):
+        mb = self.model_based_estimator.predict([np.append(x, [a])])[0]
+        return mb + (r - mb) * self.eval_policy.give_probability(x, a) / self.log_policy.give_probability(x, a)
 
     def evaluate_policy(self):
-        rewards = []
-        for (x, a, r) in self.history:
-            rewards.append(self.evaluate_one_reward(x, self.policy(x), a, r))
-        return np.array(rewards).mean()
+        expected_rewards = []
+        for (x, a, r) in self.log_policy.history:
+            expected_rewards.append(self.evaluate_one_reward(x, a, r))
+        return np.array(expected_rewards).mean()
--- src/main.py
+++ src/main.py
@@ -3,25 +3,36 @@ import random
 from abc import ABC
 
 from src.data_synthesis import create_context_vector, do_binary_vectors, create_data_from_training_file
-from src.evaluator import IPSEvaluator
+from src.evaluator import IPSEvaluator, DoublyRobustEstimator
 import numpy as np
 from scipy.stats import bernoulli
 from src.online_simulation import Simulator
 from src.policy import RandomPolicy, DeterministicPolicy
-
+from sklearn.linear_model import LinearRegression
 # path = create_context_vector()
 # context = do_binary_vectors('data/track2/simple_context', 20)
 # data = create_triples_from_context_vectors(context)
 
 
-actions = np.arange(1000)
-contexts = np.random.randint(2, size=(500, 100))
+actions = np.arange(100)
+contexts = np.random.randint(2, size=(50, 10))
 log_policy = RandomPolicy(actions)
-eval_policy = RandomPolicy(actions)
+eval_policy = DeterministicPolicy(actions)
 simulator = Simulator(contexts, actions, log_policy, eval_policy)
-simulator.simulate(1000)
+simulator.simulate(10000)
 
 
-ips_eval = IPSEvaluator(log_policy, eval_policy)
-print(ips_eval.evaluate_policy())
-print(eval_policy.mean_reward())
+def ips():
+    ips_eval = IPSEvaluator(log_policy, eval_policy)
+    print(ips_eval.evaluate_policy())
+    print(eval_policy.mean_reward())
+
+
+def dr():
+    X = np.array(list(map(lambda x: np.append(x[0], [x[1]]), log_policy.history)))
+    y = np.array(list(map(lambda x: x[2], log_policy.history)))
+    reg = LinearRegression()
+    reg.fit(X, y)
+    dr_eval = DoublyRobustEstimator(log_policy, eval_policy, reg)
+    print(dr_eval.evaluate_policy())
+    print(eval_policy.mean_reward())
